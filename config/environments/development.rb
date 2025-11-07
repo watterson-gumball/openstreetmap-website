b1b35e9d636cc1a045dd961765b6455e2e9c561f
gumball <gw2212093@gmail.com>
2025-11-07 16:14:00 +0400

    Configure app for local network use

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # config.assume_ssl = true
+  # config.force_ssl = false
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
